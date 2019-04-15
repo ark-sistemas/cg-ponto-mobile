615a07cd003140891242db08fdf1070d781cd949
Thaina Lima <thaina.agape@gmail.com>
2019-04-14 23:56:11 -0300

    #thaina - Criação da tela principal

---
+++ cgponto/app/src/main/java/senai/fatesg/com/cgponto/activity/TelaPrincialActivity.java
@@ -0,0 +1,13 @@
+package senai.fatesg.com.cgponto.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TelaPrincialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity1_tela_principal);
+    }
+}
