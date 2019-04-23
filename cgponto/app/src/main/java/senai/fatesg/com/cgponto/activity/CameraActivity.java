b0ccd158f6a7c603f4798fcfbc3a15f6532b4b4c
Rodrigolmg <rodrigo.leitemg@gmail.com>
2019-04-23 00:17:15 -0300

    Configuring CameraActivity

--- cgponto/app/src/main/java/senai/fatesg/com/cgponto/activity/CameraActivity.java
+++ cgponto/app/src/main/java/senai/fatesg/com/cgponto/activity/CameraActivity.java
@@ -1,16 +1,21 @@
 package senai.fatesg.com.cgponto.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.io.File;
@@ -18,6 +23,7 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.UUID;
 
 import senai.fatesg.com.cgponto.R;
 import senai.fatesg.com.cgponto.interfaces.InitComponent;
@@ -29,21 +35,50 @@ public class CameraActivity extends AppCompatActivity implements InitComponent {
     private static final String TAG = "CapturePicture";
     static final int REQUEST_PICTURE_CAPTURE = 1;
     protected String pictureFilePath;
+    protected String deviceIdentifier;
+    protected AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
         init();
+        getInstallationIdentifier();
+        setListeners();
     }
 
     @Override
     public void init() {
         picture = findViewById(R.id.picture);
         btnTakePicture = findViewById(R.id.btn_take_pic);
+        dialog = buildDialog();
     }
 
-    public void sendTakePictureIntent(View view) {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_PICTURE_CAPTURE && resultCode == RESULT_OK) {
+            File imgFile = new  File(pictureFilePath);
+            if(imgFile.exists())            {
+                picture.setImageURI(Uri.fromFile(imgFile));
+//                dialog.show();
+            }
+        }
+    }
+
+    private void setListeners(){
+        btnTakePicture.setOnClickListener(v -> {
+            RelativeLayout.LayoutParams params = new RelativeLayout
+                    .LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
+                    RelativeLayout.LayoutParams.MATCH_PARENT);
+            params.setMargins(0, 0, 0, 0);
+            picture.getLayoutParams().height = ViewGroup.LayoutParams.MATCH_PARENT;
+            picture.setLayoutParams(params);
+            sendTakePictureIntent();
+
+        });
+    }
+
+    private void sendTakePictureIntent() {
         if(getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
 
             Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -51,7 +86,7 @@ public class CameraActivity extends AppCompatActivity implements InitComponent {
             if (cameraIntent.resolveActivity(getPackageManager()) != null) {
                 startActivityForResult(cameraIntent, REQUEST_PICTURE_CAPTURE);
 
-                File pictureFile = null;
+                File pictureFile;
                 try {
                     pictureFile = getPictureFile();
                 } catch (IOException ex) {
@@ -74,9 +109,47 @@ public class CameraActivity extends AppCompatActivity implements InitComponent {
     private File getPictureFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss", Locale.ROOT).format(new Date());
         String pictureFile = "FACE_" + timeStamp;
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File storageDir = getExternalFilesDir(Environment.getExternalStorageState());
         File image = File.createTempFile(pictureFile, ".jpg", storageDir);
         pictureFilePath = image.getAbsolutePath();
         return image;
     }
+
+    private AlertDialog buildDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle(getString(R.string.label_dialog_title));
+        builder.setMessage(getString(R.string.label_dialog_message));
+
+        builder.setPositiveButton(R.string.label_dialog_yes, (dialog, which) -> {
+            Intent galleryIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            File f = new File(pictureFilePath);
+            Uri picUri = Uri.fromFile(f);
+            galleryIntent.setData(picUri);
+            this.sendBroadcast(galleryIntent);
+        });
+
+        builder.setNegativeButton(R.string.label_dialog_no, (dialog, which) -> {
+        });
+
+
+        return builder.create();
+    }
+
+    protected synchronized String getInstallationIdentifier() {
+        if (deviceIdentifier == null) {
+            SharedPreferences sharedPrefs = this.getSharedPreferences(
+                    "DEVICE_ID", Context.MODE_PRIVATE);
+            deviceIdentifier = sharedPrefs.getString("DEVICE_ID", null);
+            if (deviceIdentifier == null) {
+                deviceIdentifier = UUID.randomUUID().toString();
+                SharedPreferences.Editor editor = sharedPrefs.edit();
+                editor.putString("DEVICE_ID", deviceIdentifier);
+                editor.commit();
+            }
+        }
+        return deviceIdentifier;
+    }
+
+
 }
--- cgponto/app/src/main/java/senai/fatesg/com/cgponto/activity/FaceDetectionActivity.java
+++ cgponto/app/src/main/java/senai/fatesg/com/cgponto/activity/FaceDetectionActivity.java
@@ -13,20 +13,6 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.ml.vision.FirebaseVision;
-import com.google.firebase.ml.vision.common.FirebaseVisionImage;
-import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
-import com.google.firebase.ml.vision.common.FirebaseVisionPoint;
-import com.google.firebase.ml.vision.face.FirebaseVisionFace;
-import com.google.firebase.ml.vision.face.FirebaseVisionFaceContour;
-import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
-import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
-import com.otaliastudios.cameraview.CameraView;
-import com.otaliastudios.cameraview.Facing;
-import com.otaliastudios.cameraview.Frame;
-import com.otaliastudios.cameraview.FrameProcessor;
 
 import java.util.List;
 import java.util.Optional;
@@ -34,12 +20,10 @@ import java.util.Optional;
 import senai.fatesg.com.cgponto.R;
 import senai.fatesg.com.cgponto.interfaces.InitComponent;
 
-public class FaceDetectionActivity extends AppCompatActivity implements FrameProcessor, InitComponent {
+public class FaceDetectionActivity extends AppCompatActivity implements InitComponent {
 
-    protected Facing facing = Facing.FRONT;
 
     protected ImageView imageViewFaceDetec;
-    protected CameraView cameraView;
     protected SharedPreferences sp;
 
     @Override
@@ -47,239 +31,13 @@ public class FaceDetectionActivity extends AppCompatActivity implements FramePro
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_face_detection);
         init();
-        settingCameraView();
     }
 
-    @Override
-    public void process(@NonNull Frame frame) {
-
-        Optional<Integer> optionalWidth = Optional.ofNullable(frame.getSize().getWidth());
-
-        Integer width = optionalWidth.get();
-        Integer height = frame.getSize().getHeight();
-
-        FirebaseVisionImageMetadata metadata = new FirebaseVisionImageMetadata.Builder()
-                .setWidth(width)
-                .setHeight(height)
-                .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
-                .setRotation((cameraView.getFacing() == Facing.FRONT) ?
-                        FirebaseVisionImageMetadata.ROTATION_270 :
-                        FirebaseVisionImageMetadata.ROTATION_90)
-                .build();
-
-        FirebaseVisionImage visionImage = FirebaseVisionImage
-                .fromByteArray(frame.getData(), metadata);
-
-        FirebaseVisionFaceDetectorOptions options = new FirebaseVisionFaceDetectorOptions.Builder()
-                .setContourMode(FirebaseVisionFaceDetectorOptions.ALL_CONTOURS)
-                .build();
-
-        FirebaseVisionFaceDetector faceDetector = FirebaseVision.getInstance()
-                .getVisionFaceDetector(options);
-
-        faceDetector.detectInImage(visionImage).addOnSuccessListener(firebaseVisionFaces -> {
-
-            firebaseVisionFaces.forEach(face ->{
-
-                List<FirebaseVisionPoint> faceCountours = face
-                        .getContour(FirebaseVisionFaceContour.FACE).getPoints();
-
-                List<FirebaseVisionPoint> leftEyebrowTopCountours = face
-                        .getContour(FirebaseVisionFaceContour.LEFT_EYEBROW_TOP).getPoints();
-
-                List<FirebaseVisionPoint> leftEyebrowBottomCountours = face
-                        .getContour(FirebaseVisionFaceContour.LEFT_EYEBROW_BOTTOM).getPoints();
-
-                List<FirebaseVisionPoint> rightEyebrowTopCountours = face
-                        .getContour(FirebaseVisionFaceContour.RIGHT_EYEBROW_TOP).getPoints();
-
-                List<FirebaseVisionPoint> rightEyebrowBottomCountours = face
-                        .getContour(FirebaseVisionFaceContour.RIGHT_EYEBROW_BOTTOM).getPoints();
-
-                List<FirebaseVisionPoint> leftEyeCountours = face
-                        .getContour(FirebaseVisionFaceContour.LEFT_EYE).getPoints();
-
-                List<FirebaseVisionPoint> rightEyeCountours = face
-                        .getContour(FirebaseVisionFaceContour.RIGHT_EYE).getPoints();
-
-                List<FirebaseVisionPoint> noseBottomCountours = face
-                        .getContour(FirebaseVisionFaceContour.NOSE_BOTTOM).getPoints();
-
-                List<FirebaseVisionPoint> noseBridgeCountours = face
-                        .getContour(FirebaseVisionFaceContour.NOSE_BRIDGE).getPoints();
-
-                List<FirebaseVisionPoint> upperLipTopCountours = face
-                        .getContour(FirebaseVisionFaceContour.UPPER_LIP_TOP).getPoints();
-
-                List<FirebaseVisionPoint> upperLipBottomCountours = face
-                        .getContour(FirebaseVisionFaceContour.UPPER_LIP_BOTTOM).getPoints();
-
-                List<FirebaseVisionPoint> lowerLipTopCountours = face
-                        .getContour(FirebaseVisionFaceContour.LOWER_LIP_TOP).getPoints();
-
-                List<FirebaseVisionPoint> lowerLipBottomCountours = face
-                        .getContour(FirebaseVisionFaceContour.LOWER_LIP_BOTTOM).getPoints();
-
-
-                faceCountours.forEach(faceCountour ->{
-                    String keyX = getString(R.string.face_countours_X)
-                            + String.valueOf(faceCountours.indexOf(faceCountour));
-                    String keyY = getString(R.string.face_countours_Y)
-                            + String.valueOf(faceCountours.indexOf(faceCountour));
-
-                    if((sp.getFloat(keyX, 1f) == faceCountour.getX())
-                        && sp.getFloat(keyY, 1f) == faceCountour.getY()){
-                        Toast.makeText(this, "Igual", Toast.LENGTH_LONG).show();
-                    }
-
-                });
-
-//                leftEyebrowTopCountours.forEach(leftEyebrowTopCountour ->{
-//
-//                    String keyX = R.string.left_eyebrow_top_countours_X
-//                            + String.valueOf(leftEyebrowTopCountours.indexOf(leftEyebrowTopCountour));
-//                    String keyY = R.string.left_eyebrow_top_countours_Y
-//                            + String.valueOf(leftEyebrowTopCountours.indexOf(leftEyebrowTopCountour));
-//
-//                });
-//
-//                leftEyebrowBottomCountours.forEach(leftEyebrowBottomCountour ->{
-//
-//                    String keyX = R.string.left_eyebrow_bottom_countours_X
-//                            + String.valueOf(leftEyebrowBottomCountours.indexOf(leftEyebrowBottomCountour));
-//                    String keyY = R.string.left_eyebrow_bottom_countours_Y
-//                            + String.valueOf(leftEyebrowBottomCountours.indexOf(leftEyebrowBottomCountour));
-//
-//                });
-//
-//                rightEyebrowTopCountours.forEach(rightEyebrowTopCountour ->{
-//
-//                    String keyX = R.string.right_eyebrow_top_countours_X
-//                            + String.valueOf(rightEyebrowTopCountours.indexOf(rightEyebrowTopCountour));
-//                    String keyY = R.string.right_eyebrow_top_countours_Y
-//                            + String.valueOf(rightEyebrowTopCountours.indexOf(rightEyebrowTopCountour));
-//
-//                });
-//
-//                rightEyebrowBottomCountours.forEach(rightEyebrowBottomCountour ->{
-//
-//                    String keyX = R.string.right_eyebrow_bottom_countours_X
-//                            + String.valueOf(rightEyebrowBottomCountours.indexOf(rightEyebrowBottomCountour));
-//                    String keyY = R.string.right_eyebrow_bottom_countours_Y
-//                            + String.valueOf(rightEyebrowBottomCountours.indexOf(rightEyebrowBottomCountour));
-//
-//                });
-//
-//                leftEyeCountours.forEach(leftEyeCountour ->{
-//
-//                    String keyX = R.string.left_eye_countours_X
-//                            + String.valueOf(leftEyeCountours.indexOf(leftEyeCountour));
-//                    String keyY = R.string.left_eye_countours_Y
-//                            + String.valueOf(leftEyeCountours.indexOf(leftEyeCountour));
-//
-//                });
-//
-//                rightEyeCountours.forEach(rightEyeCountour ->{
-//
-//                    String keyX = R.string.right_eye_countours_X
-//                            + String.valueOf(rightEyeCountours.indexOf(rightEyeCountour));
-//                    String keyY = R.string.right_eye_countours_Y
-//                            + String.valueOf(rightEyeCountours.indexOf(rightEyeCountour));
-//
-//                });
-//
-//                noseBottomCountours.forEach(noseBottomCountour ->{
-//                    String keyX = R.string.nose_bottom_countours_X
-//                            + String.valueOf(noseBottomCountours.indexOf(noseBottomCountour));
-//                    String keyY = R.string.nose_bottom_countours_Y
-//                            + String.valueOf(noseBottomCountours.indexOf(noseBottomCountour));
-//
-//                });
-//
-//                noseBridgeCountours.forEach(noseBridgeCountour ->{
-//                    String keyX = R.string.nose_bridge_countours_X
-//                            + String.valueOf(noseBridgeCountours.indexOf(noseBridgeCountour));
-//                    String keyY = R.string.nose_bridge_countours_Y
-//                            + String.valueOf(noseBridgeCountours.indexOf(noseBridgeCountour));
-//
-//                });
-//
-//                upperLipTopCountours.forEach(upperLipTopCountour ->{
-//                    String keyX = R.string.upper_lip_top_countours_X
-//                            + String.valueOf(upperLipTopCountours.indexOf(upperLipTopCountour));
-//                    String keyY = R.string.upper_lip_top_countours_Y
-//                            + String.valueOf(upperLipTopCountours.indexOf(upperLipTopCountour));
-//
-//                });
-//
-//                upperLipBottomCountours.forEach(upperLipBottomCountour ->{
-//                    String keyX = R.string.upper_lip_bottom_countours_X
-//                            + String.valueOf(upperLipBottomCountours.indexOf(upperLipBottomCountour));
-//                    String keyY = R.string.upper_lip_bottom_countours_Y
-//                            + String.valueOf(upperLipBottomCountours.indexOf(upperLipBottomCountour));
-//
-//                });
-//
-//                lowerLipTopCountours.forEach(lowerLipTopCountour ->{
-//                    String keyX = R.string.lower_lip_top_countours_X
-//                            + String.valueOf(lowerLipTopCountours.indexOf(lowerLipTopCountour));
-//                    String keyY = R.string.lower_lip_top_countours_Y
-//                            + String.valueOf(lowerLipTopCountours.indexOf(lowerLipTopCountour));
-//
-//                });
-//
-//                lowerLipBottomCountours.forEach(lowerLipBottomCountour ->{
-//                    String keyX = R.string.lower_lip_bottom_countours_X
-//                            + String.valueOf(lowerLipBottomCountours.indexOf(lowerLipBottomCountour));
-//                    String keyY = R.string.lower_lip_bottom_countours_Y
-//                            + String.valueOf(lowerLipBottomCountours.indexOf(lowerLipBottomCountour));
-//
-//                });
-            });
-
-//            imageViewFaceDetec.setImageBitmap(null);
-//            Bitmap bitmap = Bitmap.createBitmap(width, height,
-//                    Bitmap.Config.ARGB_8888);
-//            Canvas canvas = new Canvas(bitmap);
-//
-//            //Face
-//            Paint facePaint = new Paint();
-//            facePaint.setColor(Color.RED);
-//            facePaint.setStyle(Paint.Style.STROKE);
-//            facePaint.setStrokeWidth(4f);
-//
-//            firebaseVisionFaces.forEach(face ->{
-//
-//                canvas.drawRect(face.getBoundingBox(), facePaint);
-//
-//
-//                if(cameraView.getFacing() == Facing.FRONT){
-//                    Matrix matrix = new Matrix();
-//                    matrix.preScale(-1f, 1f);
-//                    Bitmap flippedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(),
-//                            bitmap.getHeight(), matrix, Boolean.TRUE);
-//
-//                    imageViewFaceDetec.setImageBitmap(flippedBitmap);
-//                } else {
-//                    imageViewFaceDetec.setImageBitmap(bitmap);
-//                }
-//            });
-        }).addOnFailureListener(e -> {
-            imageViewFaceDetec.setImageBitmap(null);
-
-        });
-    }
 
     @Override
     public void init() {
         sp = getSharedPreferences("face", Context.MODE_PRIVATE);
         imageViewFaceDetec = findViewById(R.id.face_detection_camera_image_view);
-        cameraView = findViewById(R.id.face_detection_camera_view);
     }
 
-    private void settingCameraView(){
-        cameraView.setFacing(facing);
-        cameraView.setLifecycleOwner(this);
-        cameraView.addFrameProcessor(this);
-    }
 }
